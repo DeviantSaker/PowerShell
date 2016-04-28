95c4d89f6d4e4ef1af3c841747367deae095f005
Claire Gurman <claire.gurman@widerplan.com>
2016-04-28 14:47:50 +0100

    Initial commit

---
+++ find_remote_logs.ps1
@@ -0,0 +1,5 @@
+﻿Clear-Host
+$filetypes = "*.log"
+$path = ""
+$pattern = Read-Host "Pattern to search for?"
+Get-ChildItem -Path $path -Include $filetypes -Recurse | Select-String -Pattern $pattern | Sort-Object -Property @{Expression="Filename";Descending=$true}, @{Expression="LineNumber";Descending=$false} | Out-GridView -Title "Log search" -Wait
